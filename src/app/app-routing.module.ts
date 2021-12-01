f903c9d3e88e33b567f4fdabf3490c2442dfc3ae
krishna <krishnajj99@gmail.com>
2021-12-01 19:00:49 +0530

    initial updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,10 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from "./home/home.component";
-import {AboutComponent} from "./about/about.component";
-import {CourseComponent} from "./course/course.component";
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-    {
-        path: "",
-        component: HomeComponent
-
-    },
-    {
-        path: "about",
-        component: AboutComponent
-    },
-    {
-        path: 'courses/:id',
-        component: CourseComponent
-    },
-    {
-        path: "**",
-        redirectTo: '/'
-    }
-];
+const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
