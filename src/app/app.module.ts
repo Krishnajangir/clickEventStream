5fa9bb7ac7506ec30d0e84038d7c30350848b7ac
krishna <krishnajj99@gmail.com>
2021-12-15 10:43:45 +0530

    click stream added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
